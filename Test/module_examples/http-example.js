10800e80a329d0316758ef9c22f0b406b8335983
Pham Huy Ha <ha.phamhuy@vietis.vn>
2019-05-09 17:01:06 +0700

    Add Testing project

---
+++ Test/module_examples/http-example.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.write("<h1>Hello World</h1>");
+    res.write("<h2>Hello Http</h2>");
+    res.end();
+}).listen(8080);
---
+++ Test/module_examples/test-my-first-module.js
@@ -0,0 +1,7 @@
+const sum = require("my-first-module");
+
+console.log("Sum 100 and 150");
+
+const c = sum(100, 150);
+
+console.log("Value = " + c);
